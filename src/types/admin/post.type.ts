a7cd331cb37e3db1bb2a4a9fed46a5cd40424c9d
dungnguyen2701 <bvmldung53@gmail.com>
2025-10-12 11:19:16 +0700

    admin post type

---
+++ src/types/admin/post.type.ts
@@ -0,0 +1,36 @@
+// src/types/post.type.ts
+export type PostStatus = 'Pending' | 'Accepted' | 'Rejected'
+
+export interface PostType {
+  id: string
+  title: string
+  productName: string
+  brand: string
+  model: string
+  price: number
+  createdAt: string
+  status: PostStatus
+  priority: boolean
+}
+
+export interface PostListType {
+  post: PostType[]
+  pagination: {
+    page: number
+    limit: number
+    page_size: number
+  }
+}
+
+export interface PostListTypeConfig {
+  page?: number | string
+  limit?: number | string
+  search?: string
+  year?: string
+  status?: 'Pending' | 'Accepted' | 'Rejected'
+  priority?: string
+}
+
+export interface PostListStatus {
+  status: 'all' | 'pending' | 'published' | 'rejected' | 'certified' | 'certifying'
+}
