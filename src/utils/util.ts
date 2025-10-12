dc4608d4c27ee3aa4aeca5899fd124cbb3ca3c93
Adrew-Nguyen <nhatruong5012@gmail.com>
2025-10-12 14:48:33 +0700

    resolve refresh token

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError, HttpStatusCode } from 'axios'
+import type { ErrorResponse } from '~/types/util.type'
 
 export const isAxiosError = <T = unknown>(error: unknown): error is AxiosError<T> => axios.isAxiosError(error)
 
@@ -6,6 +7,16 @@ export const isUnprocessableEntityError = <T = unknown>(error: unknown): error i
   return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
 }
 
+export function isAxiosUnauthorizedError<UnauthorizedError>(error: unknown): error is AxiosError<UnauthorizedError> {
+  return isAxiosError(error) && error.response?.status === HttpStatusCode.Unauthorized
+}
+
+export function isAxiosExpiredTokenError<UnauthorizedError>(error: unknown): error is AxiosError<UnauthorizedError> {
+  return (
+    isAxiosUnauthorizedError<ErrorResponse<{ name: string; message: string }>>(error) &&
+    error.response?.data?.data?.name === 'EXPIRED_TOKEN'
+  )
+}
 export function formatCurrency(currency: number) {
   return new Intl.NumberFormat('de-DE').format(currency)
 }
