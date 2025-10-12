712eabee6aa672fab8e35c189328b52989062011
dungnguyen2701 <bvmldung53@gmail.com>
2025-10-12 11:30:21 +0700

    add path for account

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -7,6 +7,17 @@ export const path = {
   profile: '/profile',
   post: '/post',
   updatePhone: '/update-phone',
+  //account
+  account: '/account',
+  accountPosts: '/account/posts',
+  accountNotification: '/account/notification',
+  accountProfile: '/account/profile',
+  accountUpdateProfile: 'updateProfile',
+  accountPayment: '/account/payment',
+  accountAddress: '/account/payment',
+  accountChangePassword: '/account/password',
+  accountPrivacySetting: '/setting/privacy',
+  //admin
   admin: '/admin',
   adminDashboard: '/admin/dashboard',
   adminListings: '/admin/listings',
