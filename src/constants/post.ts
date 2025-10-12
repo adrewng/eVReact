b0b69d2df00622458e556290946059592374553a
dungnguyen2701 <bvmldung53@gmail.com>
2025-10-12 11:28:55 +0700

    postStatus as const

---
+++ src/constants/post.ts
@@ -0,0 +1,8 @@
+export const postStatus = {
+  all: 'all',
+  pending: 'pending',
+  published: 'published',
+  rejected: 'rejected',
+  certified: 'certified',
+  certifying: 'certifying'
+} as const
