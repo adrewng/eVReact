a1f44af68f5be570cd2c137909afa521e92018c8
dungnguyen2701 <bvmldung53@gmail.com>
2025-10-14 00:39:09 +0700

    done PostManagement for admin

---
+++ src/pages/PostManagement/PostManagement.tsx
@@ -0,0 +1,43 @@
+import { useQuery } from '@tanstack/react-query'
+import { isUndefined, omitBy } from 'lodash'
+import postApi from '~/apis/post.api'
+import useQueryParam from '~/hooks/useQueryParam'
+import type { PostListTypeConfig } from '~/types/admin/post.type'
+import PostStats from './components/PostStats'
+import PostFilters from './components/PostFilters'
+import PostTable from './components/PostTable'
+
+export type QueryConfig = {
+  [key in keyof PostListTypeConfig]: string
+}
+
+export default function PostManagement() {
+  const queryParams: QueryConfig = useQueryParam()
+
+  const queryConfig: QueryConfig = omitBy(
+    {
+      page: queryParams.page || 1,
+      limit: queryParams.limit,
+      search: queryParams.search,
+      year: queryParams.year,
+      status: queryParams.status,
+      priority: queryParams.priority
+    },
+    isUndefined
+  )
+
+  const { data } = useQuery({
+    queryKey: ['posts', queryConfig],
+    queryFn: () => postApi.getPostsByAdmin(queryConfig as PostListTypeConfig)
+  })
+
+  console.log('queryConfig - ', queryConfig)
+
+  return (
+    <div className='p-6 space-y-6 flex-1 overflow-y-auto'>
+      <PostStats />
+      <PostFilters queryConfig={queryConfig} />
+      <PostTable data={data} queryConfig={queryConfig} />
+    </div>
+  )
+}
