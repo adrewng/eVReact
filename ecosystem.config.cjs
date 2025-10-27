69db5fd58bcf605fc5a301f7c6a0eff35f81e1e6
nhatruong5012@gmail.com <nhatruong5012@gmail.com>
2025-10-27 17:13:03 +0700

    config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'Eviest',
-      script: 'npm run preview -- --host'
+      script: 'npm run preview -- --host',
+      env: {
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 8080
+      }
     }
   ]
 }
